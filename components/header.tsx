7cbcc93362f4dbec5d1df2892dd7f8d3694d5cec
lazy34 <maitysurajit0901@gmail.com>
2025-06-25 00:10:29 +0530

    fet: Responsive for phone

--- components/header.tsx
+++ components/header.tsx
@@ -2,9 +2,8 @@
 export function Header() {
   return (
     <div class="header">
-      <h1>Mr. Calculator</h1>
-      <p>a project by surajit maity (aka <a href="https://github.com/nextpointer/">nextpointer <img src="/assets/external-link.svg" alt="" width={12} /></a>)</p>
       <img class="hero-img" src="/assets/mr-calc.webp" alt="mr-calc" />
+      <h1>Mr. Calculator</h1>
     </div>
   );
 }
