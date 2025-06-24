d3f12915dcbaa3aba381d2ee4970fbb650e1fb37
lazy34 <maitysurajit0901@gmail.com>
2025-06-25 00:11:44 +0530

    fix: Update the contact links

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,13 +4,13 @@ export function Footer() {
     <>
       <div class="contact-section">
         <div className={"icon"}>
-          <a href="https://github.com/lazy34/Mr.-Calculator" target={"_blank"}><img src="/assets/gh.png" alt="" /></a>
+          <a href="https://github.com/nextpointer/Mr.-Calculator" target={"_blank"}><img src="/assets/gh.png" alt="" /></a>
         </div>
         <div className={"icon"}>
           <a href="https://www.linkedin.com/in/thelazycoder3112/" target={"_blank"}><img src="/assets/li.png" alt="" /></a>
         </div>
         <div className={"icon"}>
-          <a href="https://x.com/necstpointer" target={"_blank"}><img src="/assets/x.png" alt="" /></a>
+          <a href="https://x.com/nextpointerX" target={"_blank"}><img src="/assets/x.png" alt="" /></a>
         </div >
       </div>
 
