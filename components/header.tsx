0d33fe83691bcc98ceb7cc7b300771b7480dcbc2
lazy34 <maitysurajit0901@gmail.com>
2025-06-25 01:05:32 +0530

    fet: Add github starr

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,19 @@
-
 export function Header() {
   return (
-    <div class="header">
-      <img class="hero-img" src="/assets/mr-calc.webp" alt="mr-calc" />
-      <h1>Mr. Calculator</h1>
+    <div class="header-container">
+      <div class="header">
+        <img class="hero-img" src="/assets/mr-calc.webp" alt="mr-calc" />
+        <h1>Mr. Calculator</h1>
+      </div>
+      <div class="star">
+        <img class="arrow-svg" src="/assets/arrow.svg" alt="arrow" />
+        <a
+          href="https://github.com/nextpointer/Mr.-Calculator"
+          target={"_blank"}
+        >
+          <img class="star-svg" src="/assets/star.svg" alt="star-svg" />
+        </a>
+      </div>
     </div>
   );
 }
